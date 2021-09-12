b80f7ef235fee65c8bd7a52c2d69c5846a7842ae
eoskl <42143496+eoskl@users.noreply.github.com>
2021-09-12 15:16:22 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,18 +2,3 @@ variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
-
-variable "db_table_name" {
-  type    = string
-  default = "terraform-learn"
-}
-
-variable "db_read_capacity" {
-  type    = number
-  default = 1
-}
-
-variable "db_write_capacity" {
-  type    = number
-  default = 1
-}
